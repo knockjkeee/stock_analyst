573536c121d6c0dab27903f85a86718403819f8b
knockjkeee <knockjkeee@gmail.com>
2022-09-11 20:53:39 +0300

    refresh list Tickers view

--- base/src/main/java/org/rostovpavel/base/utils/Stock.java
+++ base/src/main/java/org/rostovpavel/base/utils/Stock.java
@@ -8,7 +8,7 @@ public class Stock {
     @Contract(value = " -> new", pure = true)
     @NotNull
     public static String @NotNull [] getNameTickers() {
-        return new String[]{"ASTR", "TAL", "WISH", "U", "RIOT", "LPL", "GPS", "ATVI", "SNAP", "TWTR", "MDLZ", "BAC", "JD", "KO", "INTC", "F", "AA", "CCL", "COIN", "ACH", "ET", "EQT", "FSLR", "MSTR", "PCG", "RRC", "SWN", "VIPS", "CNK", "CLOV", "ENDP", "MOMO", "PBF", "HOOD", "SPCE", "SAVE", "MOS", "UAA", "BYND", "PTON", "VALE", "RIG", "FTI", "LI", "AAL", "CCXI", "VIR", "SAVA", "ZY", "IOVA", "GTHX", "RKLB", "DKNG", "PLUG", "BBBY", "RIDE", "NOK", "RIVN", "BLUE", "SQ", "WKHS", "ATRA", "ZYNE", "TGTX", "PRAX", "T", "MARA", "MU", "PINS", "GILD", "HAL", "CVX", "V","OII"};
+        return new String[]{"ASTR", "TAL", "WISH", "U", "RIOT", "LPL", "GPS", "ATVI", "SNAP", "TWTR", "MDLZ", "BAC", "JD", "KO", "INTC", "F", "AA", "CCL", "COIN", "ACH", "ET", "EQT", "FSLR", "MSTR", "PCG", "RRC", "SWN", "VIPS", "CNK", "CLOV", "MOMO", "PBF", "HOOD", "SPCE", "SAVE", "MOS", "UAA", "BYND", "PTON", "VALE", "RIG", "FTI", "LI", "AAL", "CCXI", "VIR", "SAVA", "ZY", "IOVA", "GTHX", "RKLB", "DKNG", "PLUG", "BBBY", "RIDE", "NOK", "RIVN", "BLUE", "SQ", "WKHS", "ATRA", "ZYNE", "TGTX", "PRAX", "T", "MARA", "MU", "PINS", "GILD", "HAL", "CVX", "V","OII"};
 //        "ASTR", "TAL", "WISH", "U", "RIOT", "LPL", "GPS", "ATVI", "SNAP", "TWTR", "MDLZ", "BAC", "JD", "KO", "INTC", "BABA", "F", "AA", "CCL", "COIN", "COTY", "ET", "ENPH", "EPAM", "EQT", "FSLR", "MSTR", "PCG", "RRC", "REGI", "SEDG", "SWN", "SPOT", "VEON", "VIPS", "ZS", "CNK", "CLOV", "ENDP", "MOMO", "PBF", "HOOD", "SPCE", "AAPL", "FB", "NVDA", "SAVE", "TSLA", "DDOG", "MOS", "UAA", "BYND", "PTON", "VALE", "RIG", "FTI", "LI", "AAL", "CCXI", "VIR", "SAVA", "ZY", "DNLI"
     }
 }
