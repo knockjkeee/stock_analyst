feae6041d1a77341f99234f7364659d535aca2c4
knockjkeee <knockjkeee@gmail.com>
2022-09-11 20:54:47 +0300

    add [dev] start and stop working stockbot

--- web-service/src/main/java/org/rostovpavel/webservice/telegram/StockBot.java
+++ web-service/src/main/java/org/rostovpavel/webservice/telegram/StockBot.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
 import org.rostovpavel.base.models.Ticker;
 import org.rostovpavel.webservice.TEMPO.GenerateFile;
+import org.rostovpavel.webservice.TEMPO.MyTask;
 import org.rostovpavel.webservice.services.TickerDataService;
 import org.rostovpavel.webservice.telegram.query.UpdateHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,19 @@ import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import static org.rostovpavel.base.utils.Stock.getNameTickers;
 import static org.rostovpavel.webservice.telegram.utils.Messages.getFullInformationByTicker;
@@ -38,11 +43,16 @@ public class StockBot extends TelegramLongPollingBot{
 
     private final TickerDataService tickerDataService;
 
+    private final ScheduledExecutorService executor;
+
+    private ScheduledFuture<?> schedule;
+
     @Autowired
     private List<UpdateHandler> updateHandlers;
 
-    public StockBot(TickerDataService tickerDataService) {
+    public StockBot(TickerDataService tickerDataService, ScheduledExecutorService executor) {
         this.tickerDataService = tickerDataService;
+        this.executor = executor;
     }
 
     @Override
@@ -63,8 +73,6 @@ public class StockBot extends TelegramLongPollingBot{
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-
-
 
         if (update.hasCallbackQuery()) {
             for (UpdateHandler updateHandler : updateHandlers) {
@@ -105,7 +113,20 @@ public class StockBot extends TelegramLongPollingBot{
                 }
             });
 
-        } else if(text.length() <= 4){
+        }else if (text.equals("startbot")){
+            execute(SendMessage.builder()
+                    .chatId(String.valueOf(update.getMessage().getChatId()))
+                    .text("<pre>Поиск подходящего временного интервала...</pre>")
+                    .parseMode(ParseMode.HTML)
+                    .disableWebPagePreview(true)
+                    .replyToMessageId(update.getMessage().getMessageId())
+                    .build());
+            run();
+            init();
+        }else if (text.equals("stopbot")){
+            stop();
+            cancel();
+        }else if(text.length() <= 4){
             String query = update.getMessage()
                     .getText()
                     .toUpperCase(Locale.ROOT);
@@ -126,7 +147,7 @@ public class StockBot extends TelegramLongPollingBot{
     }
 
     @SneakyThrows
-    @PostConstruct
+//    @PostConstruct
     public void init() {
         String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         execute(SendMessage.builder()
@@ -135,5 +156,44 @@ public class StockBot extends TelegramLongPollingBot{
                 .parseMode(ParseMode.HTML)
                 .disableWebPagePreview(true)
                 .build());
+    }
+
+    @SneakyThrows
+    public void cancel() {
+        execute(SendMessage.builder()
+                .chatId(idChat)
+                .text("<pre>❗️❗️❗️ Стоп машина, на сегодня все... полна банка огурцов </pre>")
+                .parseMode(ParseMode.HTML)
+                .disableWebPagePreview(true)
+                .build());
+    }
+
+    private void run() {
+        List<String> mmStart = Arrays.asList("14", "29", "44", "59");
+        List<String> ssStart = Arrays.asList("50", "51",  "52",  "53",  "54", "55",  "56", "57", "58", "59");
+        MyTask testMytest = new MyTask("testMytest");
+
+//        ForkJoinPool executor = ForkJoinPool.commonPool();
+        //ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+//        test("test", executor);
+        while (true) {
+            String currentMM = new SimpleDateFormat("mm").format(Calendar.getInstance().getTime());
+            String currentSS = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+            if (mmStart.contains(currentMM) && ssStart.contains(currentSS)) {
+                System.out.println("Start -> " + new SimpleDateFormat("HH.mm.ss").format(Calendar.getInstance().getTime()));
+                schedule = executor.scheduleAtFixedRate(testMytest, 0, 15, TimeUnit.MINUTES);
+                break;
+            }
+            try {
+                System.out.println("Wait -> " + new SimpleDateFormat("HH.mm.ss").format(Calendar.getInstance().getTime()));
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stop() {
+        schedule.cancel(true);
     }
 }
